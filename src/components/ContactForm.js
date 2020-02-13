ad3fd00e2af333480b076dd94aa34d8a81958beb
Key Bristol <keyeric.bristol@gmail.com>
2020-02-13 21:52:05 +0000

    Fixed the 'bug' of the ContactForm not having ids. I shouldnt have to defend this test.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,6 +16,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id='firstName'
             name="firstName"
             placeholder="bill"
             ref={register({ required: true, maxLength: 3 })}
@@ -29,6 +30,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
+            id='lastName'
             placeholder="luo"
             ref={register({ required: true })}
           />
@@ -38,17 +40,22 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
-            Email*
-          </label>
-          <input name="email" ref={register({ required: true })} />
+          <label htmlFor="email">Email*</label>
+          <input 
+          name="email" 
+          id='email'
+          placeholder="bluebill1049@hotmail.com"
+          ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea 
+          name="message" 
+          id='message'
+          ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
